859ddf04cc454ef2fa922d116d5b62288a2bc1ce
ZheerBarzan <zheerbarzan85@outlook.com>
2023-08-09 10:13:50 +0300

    added the chess pieces to the board in thier corresponding position

--- lib/game_board.dart
+++ lib/game_board.dart
@@ -40,38 +40,116 @@ class _GameBoardState extends State<GameBoard> {
     }
 
     // place rooks
+    newBoard[0][0] = ChessPiece(
+      type: ChessPieceType.rook,
+      isWhite: false,
+      imagePath: "lib/images/rook.png",
+    );
+    newBoard[0][7] = ChessPiece(
+      type: ChessPieceType.rook,
+      isWhite: false,
+      imagePath: "lib/images/rook.png",
+    );
+    newBoard[7][0] = ChessPiece(
+      type: ChessPieceType.rook,
+      isWhite: true,
+      imagePath: "lib/images/rook.png",
+    );
+    newBoard[7][7] = ChessPiece(
+      type: ChessPieceType.rook,
+      isWhite: true,
+      imagePath: "lib/images/rook.png",
+    );
 
     // place knights
+    newBoard[0][1] = ChessPiece(
+      type: ChessPieceType.knight,
+      isWhite: false,
+      imagePath: "lib/images/knight.png",
+    );
+    newBoard[0][6] = ChessPiece(
+      type: ChessPieceType.knight,
+      isWhite: false,
+      imagePath: "lib/images/knight.png",
+    );
+    newBoard[7][1] = ChessPiece(
+      type: ChessPieceType.knight,
+      isWhite: true,
+      imagePath: "lib/images/knight.png",
+    );
+    newBoard[7][6] = ChessPiece(
+      type: ChessPieceType.knight,
+      isWhite: true,
+      imagePath: "lib/images/knight.png",
+    );
 
     // place bishops
+    newBoard[0][2] = ChessPiece(
+      type: ChessPieceType.bishop,
+      isWhite: false,
+      imagePath: "lib/images/bishop.png",
+    );
+    newBoard[0][5] = ChessPiece(
+      type: ChessPieceType.bishop,
+      isWhite: false,
+      imagePath: "lib/images/bishop.png",
+    );
+    newBoard[7][2] = ChessPiece(
+      type: ChessPieceType.bishop,
+      isWhite: true,
+      imagePath: "lib/images/bishop.png",
+    );
+    newBoard[7][5] = ChessPiece(
+      type: ChessPieceType.bishop,
+      isWhite: true,
+      imagePath: "lib/images/bishop.png",
+    );
 
     // place queens
+    newBoard[0][4] = ChessPiece(
+      type: ChessPieceType.queen,
+      isWhite: false,
+      imagePath: "lib/images/queen.png",
+    );
+    newBoard[7][4] = ChessPiece(
+      type: ChessPieceType.queen,
+      isWhite: true,
+      imagePath: "lib/images/queen.png",
+    );
 
     // place kings
+    newBoard[0][3] = ChessPiece(
+      type: ChessPieceType.king,
+      isWhite: false,
+      imagePath: "lib/images/king.png",
+    );
+    newBoard[7][3] = ChessPiece(
+      type: ChessPieceType.king,
+      isWhite: true,
+      imagePath: "lib/images/king.png",
+    );
 
     board = newBoard;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Scaffold(
-        backgroundColor: backgroundColor,
-        body: GridView.builder(
-          itemCount: 8 * 8,
-          physics: const NeverScrollableScrollPhysics(),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 8),
-          itemBuilder: (context, index) {
-            final int row = ((index / 8).floor());
-            final int col = (((index % 8)).ceil());
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: GridView.builder(
+        itemCount: 8 * 8,
+        physics: const NeverScrollableScrollPhysics(),
+        gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 8),
+        itemBuilder: (context, index) {
+          final int row = ((index / 8).floor());
+          final int col = (((index % 8)).ceil());
 
-            return Squre(
-              isWhite: isWhite(index),
-              piece: board[row][col],
-            );
-          },
-        ),
+          return Squre(
+            isWhite: isWhite(index),
+            piece: board[row][col],
+          );
+        },
       ),
     );
   }
