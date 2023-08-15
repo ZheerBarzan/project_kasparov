8cf2c20e337cdae2651f6a0b2ae86f04ea239640
ZheerBarzan <zheerbarzan85@outlook.com>
2023-08-15 21:12:22 +0300

    add valid moves for the bishop  piece in the game and showing the valid moves as well

--- lib/game_board.dart
+++ lib/game_board.dart
@@ -243,6 +243,36 @@ class _GameBoardState extends State<GameBoard> {
         }
         break;
       case ChessPieceType.bishop:
+        // bishop can move to the end of the board only diagonaly
+        var directions = [
+          [-1, -1], //up left
+          [-1, 1], // up right
+          [1, -1], // down left
+          [1, 1] // down right
+        ];
+
+        for (var direction in directions) {
+          var i = 0;
+          while (true) {
+            var newRow = row + i * direction[0];
+            var newColumn = column + i * direction[1];
+            // if its out of the board stop
+            if (!isInBoard(newRow, newColumn)) {
+              break;
+            }
+            // a piece is detected
+            if (board[newRow][newColumn] != null) {
+              // if not the same color
+              if (board[newRow][newColumn]!.isWhite != piece.isWhite) {
+                candidateMoves.add([newRow, newColumn]); // kill
+              }
+              break;
+            }
+            candidateMoves.add([newRow, newColumn]);
+            i++;
+          }
+        }
+
         break;
       case ChessPieceType.queen:
         break;
